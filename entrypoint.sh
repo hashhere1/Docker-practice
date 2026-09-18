39e6bff6551ab9886c89c2916f6a9db20d1314e5
hashhere1 <aleemhassaan70@gmail.com>
2026-09-18 17:47:14 +0500

    Added celery and celery beat

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-echo "Running alembic migration"
-alembic upgrade head
-
 if [ "$#" -gt 0 ]; then
     echo "Executing custom command: $@"
     exec "$@"
-else
-    echo "Starting Fastapi server"
-    exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
 fi
+
+echo "Running alembic migration..."
+alembic upgrade head
+
+echo "Starting FastAPI server..."
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
